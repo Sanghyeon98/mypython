1aeed5ed60cd6e03448f9ea1c67bdde8a130fd0b
sang hyeon <ppsh0703@daum.net>
2024-03-12 20:35:28 +0900

    파이썬 공부시작!

---
+++ encryPython.py
@@ -0,0 +1,10 @@
+# print(chr(ord('A') *9))
+
+# pw ="a1b2c3"
+# en_pw= ""
+
+# for i in pw :
+#   en_pw +=chr(ord(i) * 9)
+
+#   print("기존 비밀번호 : %s" %pw)
+#   print("암호화된 비밀번호 : %s" %en_pw)
---
+++ hello.py
@@ -0,0 +1,21 @@
+print("hello world!");
+print("나는 박상현입니다");
+
+
+string ='안녕 나는 상현';
+print(string);
+
+score = 85;
+
+if score >=90:
+    print('A grade');
+elif score >= 80:
+    print('B grade');
+else :
+    print('알수 없음');
+
+ 
+fruits = ["사과","딸기","대방어"];
+ 
+for fruit in fruits:
+    print(fruit);
