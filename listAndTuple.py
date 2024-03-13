541b00c6e9b8a432c1b8a901b99da1c5e11e5732
sang hyeon <ppsh0703@daum.net>
2024-03-13 21:13:22 +0900

    튜플 딕셔너리 공부중

---
+++ listAndTuple.py
@@ -0,0 +1,18 @@
+  # mutable(변할 수 있는) : list
+
+  # inmmutable(변할수 없는): tuple
+  
+dataList1 = [1,2,3]
+dataList2 = dataList1
+
+dataList2.append(4)
+
+print(dataList1)
+
+dataTuple1 =(1,2,3)
+dataTuple2 =1,2,3
+  
+#튜플의 값을 수정할수없다.
+# dataTuple2[0] =10 
+print(dataList1[0])
+
