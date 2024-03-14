7f75a3b0d50433868e349c3a23eab5a237d3dd99
sang hyeon <ppsh0703@daum.net>
2024-03-14 21:28:45 +0900

    딕셔너리 예제

---
+++ dictTest.py
@@ -0,0 +1,8 @@
+numDict = {"even": [2,4,6],"odd":[1,3,5]}
+for i in numDict["even"]:
+  print(i)
+numListDict = {"1학년": [[30,40,50], [80,90,100]]}
+for i in numListDict["1학년"]:
+  for j in i:
+    print(j)  
+  print("=========")  
